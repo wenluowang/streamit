5ed8b53cd20c0776abfdeff98829dd84d6594928
wenluowang <wangwenluo@yeah.net>
2023-03-19 02:49:33 +0000

    6

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ def main():
 
     if uploaded_file:
         # Read the uploaded CSV file into a pandas dataframe
-        df = pd.read_csv(uploaded_file, encoding='gbk')
+        df = pd.read_csv(uploaded_file, encoding='gb18030')
 
         # Display the uploaded data in a table
         st.dataframe(df)
