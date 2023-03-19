572d6e2b2ebc2db85fa200df36216819822d5508
wenluowang <wangwenluo@yeah.net>
2023-03-19 02:52:08 +0000

    7

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 import pandas as pd
+from googletrans import Translator
+
+def translate_country_names(df):
+    translator = Translator()
+
+    # Create a new column to store the translated country names
+    df['Country (EN)'] = df['国家'].apply(lambda x: translator.translate(x, dest='en').text)
+
+    return df
 
 def main():
     st.title('CSV File Viewer')
@@ -10,6 +19,9 @@ def main():
     if uploaded_file:
         # Read the uploaded CSV file into a pandas dataframe
         df = pd.read_csv(uploaded_file, encoding='gb18030')
+
+        # Translate the country names and add a new column to the dataframe
+        df = translate_country_names(df)
 
         # Display the uploaded data in a table
         st.dataframe(df)
